f4843467b2a9f511fe2f8796fdd0a3d89fbb4d7c
Sparky333 <2015jasonl@gmail.com>
2024-11-16 13:38:41 -0500

    Add agenda reading functionality
    Skiprows is 14 assuming input files are of the same format as the template agenda provided in the starter code.

--- import_agenda.py
+++ import_agenda.py
@@ -3,6 +3,13 @@ import argparse
 import os
 import pandas as pd
 
+def read_agenda(agenda_file):
+    try:
+        df = pd.read_excel(agenda_file, skiprows=14, engine='xlrd')
+        print(df.head())
+    except Exception as e:
+        print(f"Exception while reading the Excel file: {e}")
+
 def main():
     parser = argparse.ArgumentParser(description='Agenda Importer')
     parser.add_argument('agenda', type=str, required=True,
