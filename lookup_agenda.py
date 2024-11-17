3ffe88777cd183509d28b762fe07cd4bfca8f58a
Sparky333 <2015jasonl@gmail.com>
2024-11-17 14:04:14 -0500

    Add lookup functionality except for speaker

--- lookup_agenda.py
+++ lookup_agenda.py
@@ -8,19 +8,48 @@ TODO:
 Add unit tests:
 title break should give len 7 results
 title breakfast should give len 3 results
+location "coral loune" should give 7
+location "lobby" should give 3 results
 
+search for debate should give one subsession
 '''
 
 def lookup_speaker(value):
     pass
 def lookup(column, value):
-    sessions =              db_table("sessions", schemas.SESSIONS_SCHEMA)
-    subsessions =           db_table("subsessions", schemas.SUBSESSIONS_SCHEMA)
-    speaker_to_session =    db_table("speaker_to_session", schemas.SPEAKER_TO_SESSION_SCHEMA)
+    sessions              = db_table("sessions", schemas.SESSIONS_SCHEMA)
+    subsessions           = db_table("subsessions", schemas.SUBSESSIONS_SCHEMA)
+    speaker_to_session    = db_table("speaker_to_session", schemas.SPEAKER_TO_SESSION_SCHEMA)
     speaker_to_subsession = db_table("speaker_to_subsession", schemas.SPEAKER_TO_SUBSESSION_SCHEMA)
+    output                = ""
 
-    print(len(sessions.select(["date", "time_start", "time_end", "type", "title", "location", "description", "speakers"], {column: value})))
+    # search for sessions that match
+    session_match = sessions.select(where = {column: value})
 
+    matchedSubsessionIDs = []   # used to ensure no duplicate subsessions are outputted
+    # for each matched session, search for subsesions of that session
+    for sess in session_match:
+        output += str(sess) + "\n"
+
+        subsess_of_sess = subsessions.select(["subsessionid","date", "time_start", "time_end", "type", "title", "location", "description", "speakers"], {"parent_session":sess["sessionid"]})
+        matchedSubsessionIDs = [subsess["subsessionid"] for subsess in subsess_of_sess]
+
+        for subsess in subsess_of_sess:
+            output += str(subsess) + "\n"
+
+    matchedSubsessionIDs = set(matchedSubsessionIDs)
+
+    # search for subsessions that match (their parent session didn't match)
+    subsession_match = subsessions.select(["subsessionid","date", "time_start", "time_end", "type", "title", "location", "description", "speakers"], {column: value})
+    for subsess in subsession_match:
+        # if subsess was already added because its parent session was matched, skip
+        if subsess["subsessionid"] in matchedSubsessionIDs: 
+            continue
+
+        for s in subsess:
+            output += str(s) + "\n"
+
+    print(output)
 
 def main():
     parser = argparse.ArgumentParser(description='Agenda Lookup')
